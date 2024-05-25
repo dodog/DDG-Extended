6750ab82504f1439cde089e873249555d4d85d2e
Jozef Gaal <dodog@users.noreply.github.com>
2024-05-26 00:54:30 +0200

    Update DuckduckGo_MExtended.meta.js

--- src/DuckduckGo_MExtended.meta.js
+++ src/DuckduckGo_MExtended.meta.js
@@ -1,14 +1,16 @@
 // ==UserScript==
-// @name            DuckDuckAdvanceMenu
-// @description     Extends DuckDuckGo by adding a customizable list of additional search engines for making fast searches from other engines and make anime search faster.
-// @homepage        https://github.com/Sb2001nov/Advance_DDG/
-// @icon            https://raw.githubusercontent.com/Sb2001nov/Advance_DDG/main/resources/large.png
+// @name            DuckDuckGO - Extended 5
+// @description     Extends DuckDuckGo by adding a customizable list of additional search engines for making fast searches from other engines.
+// @homepage        https://github.com/dodog/DDG-Extended
+// @icon            
 // @include         *://duckduckgo.com/?q=*
-// @require         //ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
+// @match           *://duckduckgo.com/*
+// @require         *://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @grant           GM_xmlhttpRequest
 // @license         MIT
-// @version         1.0.0
+// @version         1.0.1
 // @author          Shubham Singh
 // ==/UserScript==
+//[Based on DuckduckMenu script from Shubham Singh](https://github.com/ishubhamsingh2e/Advance_DDG)
