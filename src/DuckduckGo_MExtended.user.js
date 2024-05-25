5c9f1dfb4fc8f73848cae2542bfba62615b83ee3
Jozef Gaal <dodog@users.noreply.github.com>
2024-05-26 01:40:23 +0200

    Update DuckduckGo_MExtended.user.js

--- src/DuckduckGo_MExtended.user.js
+++ src/DuckduckGo_MExtended.user.js
@@ -2,7 +2,7 @@
 // @name            DuckDuckGO - Extended 5
 // @description     Extends DuckDuckGo by adding a customizable list of additional search engines for making fast searches from other engines.
 // @homepage        https://github.com/dodog/DDG-Extended
-// @icon            https://raw.githubusercontent.com/dodog/DDG-Extended/main/resources/large.png
+// @icon            *://raw.githubusercontent.com/dodog/DDG-Extended/main/resources/large.png
 // @include         *://duckduckgo.com/?q=*
 // @match           *://duckduckgo.com/*
 // @require         https:://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
@@ -10,7 +10,7 @@
 // @grant           GM_setValue
 // @grant           GM_xmlhttpRequest
 // @license         MIT
-// @version         1.0.2
+// @version         1.0.3
 // @updateURL       https://github.com/dodog/DDG-Extended/raw/main/src/DuckduckGo_MExtended.user.js
 // @author          Shubham Singh
 // ==/UserScript==
